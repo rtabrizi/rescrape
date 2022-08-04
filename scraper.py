62b15d38f1b6892d307a58e0cf58e1608d84212b
Ryan Tabrizi <rtabrizi@berkeley.edu>
2022-08-03 23:21:42 -0700

    successful scraping, still slow and occasional error last seen on file 118

--- scraper.py
+++ scraper.py
@@ -1,23 +1,46 @@
 import bs4 as bs
 import urllib.request
+import requests
+import re
+import io
+from PyPDF2 import PdfFileReader
 
 url = 'https://openaccess.thecvf.com/CVPR2022?day=all'
 sauce = urllib.request.urlopen(url).read()
 soup = bs.BeautifulSoup(sauce, 'lxml')
 links = soup.find_all('a', href=True)
 paperLinks = []
-
-
-def contains(substring, string):
-    if substring.lower() in string.lower():
-        return True
-    else:
-        return False
-
+domain = "https://openaccess.thecvf.com"
 for link in links:
     if link.text == 'pdf':
-        paperLinks.append(link)
-
-for paper in paperLinks:
-    print(paper)
-    print()
+        paperLinks.append(domain + link['href'])
+results = []
+def info(pdf_path, results):
+ 
+    # used get method to get the pdf file
+    response = requests.get(pdf_path)
+ 
+    # response.content generate binary code for
+    # string function
+    with io.BytesIO(response.content) as f:
+        # initialized the pdf
+        pdf = PdfFileReader(f)
+        firstPage = pdf.getPage(0)
+        text = firstPage.extractText()
+        header = text[:400]
+        footNote = text[-100:]
+        pos1 = "UC Berkeley"
+        pos2 = "University of California, Berkeley"
+        inHeader = pos1 in header or pos2 in header
+        inFootNote = pos1 in footNote or pos2 in footNote
+        if inHeader or inFootNote and pdf_path not in results:
+            print("MATCH FOUND!")
+            results.append(pdf_path)
+            with open('outputs.txt', 'a') as outputFile:
+                outputFile.write(f"{pdf_path}\n")
+                outputFile.close()
+counter = 1
+for i in paperLinks:
+    info(i, results)
+    print(counter)
+    counter += 1
