52801c5d6d288a7744b26997500e6874b741ab39
Ryan Tabrizi <rtabrizi@berkeley.edu>
2022-08-04 22:24:28 -0700

    added .yml, CVPR <2021 not working due to website structure

---
+++ main.py
@@ -0,0 +1,3 @@
+from scraper import scrapeConference
+
+scrapeConference('CVPR', 2020, ['UC Berkeley', 'University of California, Berkeley', 'Berkeley'])
--- scraper.py
+++ scraper.py
@@ -1,25 +1,49 @@
 import bs4 as bs
 import urllib.request
 import requests
-import re
 import io
 from PyPDF2 import PdfFileReader
 
-url = 'https://openaccess.thecvf.com/CVPR2022?day=all'
-sauce = urllib.request.urlopen(url).read()
-soup = bs.BeautifulSoup(sauce, 'lxml')
-links = soup.find_all('a', href=True)
-paperLinks = []
-domain = "https://openaccess.thecvf.com"
-for link in links:
-    if link.text == 'pdf':
-        paperLinks.append(domain + link['href'])
-results = []
-def info(pdf_path, results):
- 
-    # used get method to get the pdf file
+
+# url = 'https://openaccess.thecvf.com/CVPR2021?day=all'
+# url2 = "https://www.ecva.net/papers.php"
+# sauce = urllib.request.urlopen(url).read()
+# soup = bs.BeautifulSoup(sauce, 'lxml')
+# links = soup.find_all('a', href=True)
+# paperLinks = []
+# domain = "https://openaccess.thecvf.com"
+# for link in links:
+#     if link.text == 'pdf':
+#         paperLinks.append(domain + link['href'])
+# results = []
+
+def scrapeConference(conference, year, queries):
+    if conference == 'CVPR':
+        print('CVPR')
+        scrapeCVPR(year, queries)
+
+
+def scrapeCVPR(year, queries):
+    url = 'https://openaccess.thecvf.com/CVPR%s?day=all' % year
+    sauce = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    if year >= 2021:
+        links = soup.find_all('a', href=True)
+        domain = "https://openaccess.thecvf.com"
+        results = []
+        pdfCount = 1
+        for link in links:
+            if link.text == 'pdf':
+                link = domain + link['href']
+                print(pdfCount)
+                findQuery(link, year, queries, results)
+                pdfCount += 1
+    else:
+        print("not yet accounted for")
+
+def findQuery(pdf_path, year, queries, results):
+    # used get method to get the pdfile
     response = requests.get(pdf_path)
- 
     # response.content generate binary code for
     # string function
     with io.BytesIO(response.content) as f:
@@ -29,18 +53,9 @@ def info(pdf_path, results):
         text = firstPage.extractText()
         header = text[:400]
         footNote = text[-100:]
-        pos1 = "UC Berkeley"
-        pos2 = "University of California, Berkeley"
-        inHeader = pos1 in header or pos2 in header
-        inFootNote = pos1 in footNote or pos2 in footNote
-        if inHeader or inFootNote and pdf_path not in results:
-            print("MATCH FOUND!")
+        if any([query in header or query in footNote for query in queries]):
+            print("MATCH FOUND: %s" % pdf_path)
             results.append(pdf_path)
-            with open('outputs.txt', 'a') as outputFile:
+            with open('CVPR%soutputs.txt' % str(year), 'a') as outputFile:
                 outputFile.write(f"{pdf_path}\n")
                 outputFile.close()
-counter = 1
-for i in paperLinks:
-    info(i, results)
-    print(counter)
-    counter += 1
